4b62ad893e5bf1c28247d43de8f752b6c4d15199
kjes9307 <kjes9307@gmail.com>
2022-12-11 14:54:34 +0800

    update layout

--- src/auth/index.tsx
+++ src/auth/index.tsx
@@ -1,8 +1,7 @@
 import { ReactQueryDevtools } from 'react-query/devtools'
 import { Route, Routes } from "react-router";
-import { Container,Row, Col } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import { ProjectModal,DeleteNote } from "component/modal"
-
 import {AuthHeader} from 'component/auth-header';
 import {Task} from 'auth/Homepage'
 import {Board} from 'auth/board'
@@ -11,40 +10,11 @@ import {TaskBoard} from 'auth/TaskBoard'
 import {TeamUp} from 'auth/Team';
 import {Group} from 'auth/Team/group'
 import {Invites} from 'auth/Team/invite'
-import {IconName} from '@fortawesome/fontawesome-common-types';
-
-import Icon from 'component/Icon';
-import { useState } from 'react';
 export const Main =() =>{
-    const [colapse,setColapse] = useState(false)
-    let array:{name:string,icon:IconName}[] = [
-        {name:'project',icon:'diagram-project'},
-        {name:'member',icon:"user"},
-        {name:"invite",icon:"envelope"}
-    ]
     return (
         <>
             <AuthHeader />
             <Container fluid>
-                <Row>
-                <Col xs='2'>
-                <ul className={`sidebar list-unstyled text-white position-relative border ${!colapse?'w-150':'w-40'}`}>
-                    {array?.map(i=>{return(
-                        <li key={i.name} className={`p-2 mt-3 mouseIn d-flex ${!colapse ? 'justify-content-start':'justify-content-center'} align-items-center`}>
-                            <Icon icon={i.icon} color='white' size='1x' className={!colapse ?'ms-2 me-3' :''} />
-                            { !colapse &&<p className='text-size mb-0'>{i.name}</p> }
-                        </li>
-                    )})}
-                    <Icon 
-                        onClick={()=>setColapse(!colapse)} 
-                        icon={!colapse?'square-caret-left':'square-caret-right'} 
-                        color='white' 
-                        size='1x' 
-                        className='position-absolute top-0 start-100 translate-middle mt-2' 
-                    />
-                </ul>
-                </Col>
-                <Col xs='10'>
                 <Routes>
                     <Route index element={<Task />} ></Route>
                     <Route path='task/:id' element={<Board />}>
@@ -52,14 +22,12 @@ export const Main =() =>{
                         <Route path='Event' element={<TaskBoard />} /> 
                         <Route index element={<TaskBoard />} />
                     </Route>
-                    <Route path='team' element={<TeamUp />}>
+                    {/* <Route path='team' element={<TeamUp />}>
                         <Route path='group' element={<Group />} />
                         <Route path='invites' element={<Invites />} />
                         <Route index element={<Group />} />
-                    </Route>
+                    </Route> */}
                 </Routes>
-                </Col>
-                </Row>
             </Container>
             <ProjectModal projectModalOpen={false} />
             <DeleteNote deleteModalOpen={false} />
---
+++ src/auth/pageLayout.tsx
@@ -0,0 +1,59 @@
+import Icon from "component/Icon"
+import {IconName} from '@fortawesome/fontawesome-common-types';
+import { useState,FC, ReactNode } from 'react';
+interface PropsType{
+    children : ReactNode
+}
+const PageLayout:FC<PropsType> = ({children}) =>{
+    const [show, setShow] = useState(false);
+    let array:{name:string,icon:IconName}[] = [
+        {name:'project',icon:'diagram-project'},
+        {name:'member',icon:"user"},
+        {name:"invite",icon:"envelope"}
+    ]
+    return (
+        <div className="app-layout">
+            <div className={show ? 'sidebar-layout space-toggle-back' : 'sidebar-layout space-toggle-forward'}>
+                <div className={`sidebar-header bg-brand-font fs-2 ${show ? 'space-toggle-back' : 'space-toggle-forward'}`}>
+                    <Icon icon='check-double' size='1x' />
+                    <span>MY PROJECT</span>
+                </div>
+                <ul className={`sidebar text-white ${show ? 'show' :""} mb-0`}>
+                    {/* <li className="d-flex justify-content-center" >
+                        <Icon icon='shuttle-space' size="2x" color="white"/>
+                    </li> */}
+                    {array?.map(i=>{return(
+                        <li key={i.name} 
+                            className={`
+                                d-flex align-items-center mt-3 mouseIn ${!show ? 'justify-content-start' : 'justify-content-center'}`
+                            }>
+                            <div 
+                                className={`
+                                    icon-size ${show ? 'd-flex align-items-center justify-content-center':'ms-2'}`
+                            }>
+                                <Icon icon={i.icon} color='white' size='1x' />
+                            </div>
+                            {
+                                !show ?<span className="ms-2">{i.name}</span> : null
+                            }
+                        </li>  
+                    )})}
+                </ul>
+                <div className="position-relative border border-black content-body">
+                    <Icon 
+                        onClick={()=>setShow(!show)} 
+                        icon={!show?'square-caret-left':'square-caret-right'} 
+                        color='white' 
+                        size='1x' 
+                        className='position-absolute icontranslate' 
+                    />
+                    <div>
+                    {children}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default PageLayout
